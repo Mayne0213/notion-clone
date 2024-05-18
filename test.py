0523ad68c6646a52ce8fe012dbbbdc8d082f9d56
김민조 <150824925+Mayne0213@users.noreply.github.com>
2024-05-19 07:43:46 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-v,e = map(int,input().split())
-startNode = int(input())
-graph = [list() for _ in range(v+1)]
-for _ in range(e):
-    u,v,w = map(int,input().split())
-    graph[u].append((v,w))
-
-print("it has beed added newly")
